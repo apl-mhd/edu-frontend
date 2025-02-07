9739809ec498c8f6f9a241d3ffe9778020316339
Apel Mahmud <apl.mhd@gmail.com>
2025-02-07 11:39:46 +0600

    project initialize

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <b-button class="btn-success">apel</b-button>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
